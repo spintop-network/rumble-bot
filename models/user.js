20a922dfe2549344f6c6313758615bdca7ff2f4a
Enes izgi <enesizgi@pm.me>
2023-07-13 14:23:25 +0300

    feat: add register command

---
+++ models/user.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema(
+  {
+    discord_id: String,
+    wallet_id: String,
+    health_points: Number,
+    attack_power: Number,
+    energy_points: Number,
+    health_potion_cost: Number,
+    gold: Number,
+    weapon: String,
+    armor: String
+  },
+  {
+    typeKey: '$type',
+    timestamps: { createdAt: 'doc_created_at', updatedAt: 'doc_updated_at' }
+  }
+);
+
+UserSchema.index({ discord_id: 1 }, { unique: true });
+module.exports = mongoose.model('User', UserSchema, 'users');
