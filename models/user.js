2fa871fdc2adab154be4da100101673a542707fb
Enes izgi <enesizgi@pm.me>
2023-07-24 20:22:14 +0300

    chore: unique wallet id

--- models/user.js
+++ models/user.js
@@ -19,4 +19,5 @@ const UserSchema = new mongoose.Schema(
 );
 
 UserSchema.index({ discord_id: 1 }, { unique: true });
+UserSchema.index({ wallet_id: 1 }, { unique: true });
 module.exports = mongoose.model('User', UserSchema, 'users');
