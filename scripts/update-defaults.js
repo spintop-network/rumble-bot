bf887807d632786e5f5715350b504ce4843cec8b
Enes izgi <enesizgi@pm.me>
2023-08-09 13:32:11 +0300

    feat: add update defaults script

---
+++ scripts/update-defaults.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const process = require('process');
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URI);
+
+const {
+  BASE_ENERGY_POINTS,
+  BASE_ATTACK_POWER,
+  STARTER_GOLD
+} = require('../constants');
+
+const User = require('../models/user');
+
+(async () => {
+  const session = await mongoose.startSession();
+  try {
+    await session.withTransaction(async () => {
+      await User.updateMany(
+        {},
+        {
+          $set: {
+            gold: STARTER_GOLD,
+            energy_points: BASE_ENERGY_POINTS,
+            attack_power: BASE_ATTACK_POWER
+          }
+        }
+      );
+    });
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await session.endSession();
+    process.exit(0);
+  }
+})();
