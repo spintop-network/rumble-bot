aad58a9debadc1ea549d1a912aa8ffc0e0897278
Enes izgi <enesizgi@pm.me>
2023-08-14 12:45:02 +0300

    feat: show remaining players on leaderboard

--- buttons/leaderboard.js
+++ buttons/leaderboard.js
@@ -107,24 +107,29 @@ const leaderboard = async (interaction) => {
     } else {
       users = aliveUsers.slice(0, 20);
     }
+    const remaining_player_count = await User.countDocuments({
+      health_points: { $gt: 0 }
+    });
     await interaction.reply({
       embeds: [
-        new EmbedBuilder()
-          .setTitle('Leaderboard')
-          .setDescription(
+        new EmbedBuilder().setDescription(
+          bold('Remaining Players: ') +
+            `${remaining_player_count}\n\n` +
+            bold('Leaderboard') +
+            '\n\n' +
             bold('Pilot Name | Health Points | Kill Count | Damage Inflicted') +
-              '\n\n' +
-              users
-                .map(
-                  (user, index) =>
-                    `${index + 1}. ${userMention(user.discord_id)} | ${
-                      user.health_points
-                    } | ${user.stats.kills ?? 0} | ${
-                      user.stats.inflicted_damage ?? 0
-                    }`
-                )
-                .join('\n')
-          )
+            '\n\n' +
+            users
+              .map(
+                (user, index) =>
+                  `${index + 1}. ${userMention(user.discord_id)} | ${
+                    user.health_points
+                  } | ${user.stats.kills ?? 0} | ${
+                    user.stats.inflicted_damage ?? 0
+                  }`
+              )
+              .join('\n')
+        )
       ],
       ephemeral: true
     });
