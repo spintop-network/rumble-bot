7bbe1cb6b80aa0870af20f5ebf7d6ec6228032e7
Enes izgi <enesizgi@pm.me>
2023-10-31 15:04:30 +0300

    feat: show user always on leaderboard

--- buttons/leaderboard.js
+++ buttons/leaderboard.js
@@ -47,9 +47,6 @@ const leaderboard = async (interaction) => {
           'stats.kills': -1,
           'stats.inflicted_damage': -1
         }
-      },
-      {
-        $limit: 20
       }
     ]);
     if (aliveUsers.length < 20) {
@@ -98,18 +95,25 @@ const leaderboard = async (interaction) => {
             'stats.kills': -1,
             'stats.inflicted_damage': -1
           }
-        },
-        {
-          $limit: 20
         }
       ]);
-      users = [...aliveUsers, ...deadUsers].slice(0, 20);
+      users = [...aliveUsers, ...deadUsers];
     } else {
-      users = aliveUsers.slice(0, 20);
+      users = aliveUsers;
     }
     const remaining_player_count = await User.countDocuments({
       health_points: { $gt: 0 }
     });
+    const first_20_users = users.slice(0, 20);
+    const is_user_displayed = first_20_users?.find(
+      (user) => user.discord_id === interaction.user.id
+    );
+    let currentUserIndex;
+    if (!is_user_displayed) {
+      currentUserIndex = users.findIndex(
+        (user) => user.discord_id === interaction.user.id
+      );
+    }
     await interaction.reply({
       embeds: [
         new EmbedBuilder().setDescription(
@@ -119,16 +123,25 @@ const leaderboard = async (interaction) => {
             '\n\n' +
             bold('Pilot Name | Health Points | Kill Count | Damage Inflicted') +
             '\n\n' +
-            users
-              .map(
+            [
+              ...first_20_users.map(
                 (user, index) =>
                   `${index + 1}. ${userMention(user.discord_id)} | ${
                     user.health_points
                   } | ${user.stats.kills ?? 0} | ${
                     user.stats.inflicted_damage ?? 0
                   }`
-              )
-              .join('\n')
+              ),
+              ...(!is_user_displayed && currentUserIndex !== -1
+                ? [
+                    `${currentUserIndex + 1}. ${userMention(
+                      users[currentUserIndex].discord_id
+                    )} | ${users[currentUserIndex].health_points} | ${
+                      users[currentUserIndex].stats.kills ?? 0
+                    } | ${users[currentUserIndex].stats.inflicted_damage ?? 0}`
+                  ]
+                : [])
+            ].join('\n')
         )
       ],
       ephemeral: true
